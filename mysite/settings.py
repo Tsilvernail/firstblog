9e15522aa5747c6fb1a0aa208cbc83b7b30b9a41
Tsilvernailnail <tsilvernail@gmail.com>
2017-02-06 00:41:30 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rfgq@s*8$(wlu3znon5uxxb%3c2$97ne1t5vf*#7lt$vd_o4c2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'tsilvernail.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
