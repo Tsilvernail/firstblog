5880c330f124f660b8c1dc5bccfb7e8bb1b561d3
timm <tsilvernail@gmail.com>
2017-02-07 18:46:40 -0500

    Modified templates to display posts from database, filtered by author.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your views here.
+me = User.objects.get(username='timm')
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(author =me).order_by('author')
+    return render(request, 'blog/post_list.html', {'posts': posts})
